0292bc76fa437d819a45c1868730a959e22ff72b
Yigit1708 <yigitcan.turan@std.yeditepe.edu.tr>
2025-12-22 23:29:52 +0300

    merge

--- assemblyproject/main.c
+++ assemblyproject/main.c
@@ -1,74 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include "parser.h"
-#include "tables.h"
-#include "pass1.h"
 
 int main() {
-    FILE *fp = fopen("test_main.asm", "r");
+    FILE *fp = fopen("input.asm", "r");
     if (!fp) {
         printf("input.asm dosyasi acilamadi\n");
         return 1;
     }
 
-    // .s dosyasını aç (partial code için)
-    FILE *sfp = fopen("output.s", "w");
-    if (!sfp) {
-        printf("output.s dosyasi olusturulamadi\n");
-        fclose(fp);
-        return 1;
-    }
-
-    // Tabloları başlat
-    initTables();
-
     char line[100];
     ParsedLine pl;
     int LC = 0;
-    int isStartFound = 0;
-
-    printf("=== PASS 1 - PARSING VE TABLO OLUŞTURMA ===\n\n");
 
     while (fgets(line, sizeof(line), fp)) {
-        // Boş satırları ve yorumları atla
-        int len = strlen(line);
-        if (len > 0 && line[len-1] == '\n') line[len-1] = '\0';
-        
         int res = parseLine(line, &pl);
         if (res <= 0) continue;
-
-        // PROG pseudo-op (LC'yi etkilemez)
-        if (strcmp(pl.opcode, "PROG") == 0) {
-            printf("PROG: %s\n", pl.operand);
-            // HDRM Table'a H (Header) kodu ekle
-            addToHDRMTable('H', pl.operand, 0);
-            continue;
-        }
 
         // START pseudo-op
         if (strcmp(pl.opcode, "START") == 0) {
             LC = 0;
-            isStartFound = 1;
-            printf("START\n");
-            continue;
-        }
-
-        // END pseudo-op
-        if (strcmp(pl.opcode, "END") == 0) {
-            printf("END\n");
-            break;
-        }
-
-        // EXTREF ve ENTRY pseudo-ops (LC'yi etkilemez)
-        if (strcmp(pl.opcode, "EXTREF") == 0 || strcmp(pl.opcode, "ENTRY") == 0) {
-            printf("%s: %s\n", pl.opcode, pl.operand);
-            processPass1(&pl, LC, sfp);
-            continue;
-        }
-
-        // START bulunmadan instruction işleme
-        if (!isStartFound) {
-            printf("UYARI: START bulunmadan instruction bulundu, atlaniyor\n");
             continue;
         }
 
@@ -81,37 +32,14 @@ int main() {
                pl.opcode,
                strlen(pl.operand) ? pl.operand : "-");
 
-        // Pass 1 işlemleri: Tabloları güncelle ve partial code üret
-        processPass1(&pl, LC, sfp);
+        // Pass 1'in diğer modülleri buradan devam edecek
+        // updateSymbolTable(pl);
+        // updateFRT(pl);
+        // generatePartialCode(pl);
 
         LC += pl.size;
     }
 
     fclose(fp);
-    fclose(sfp);
-
-    // Tabloları yazdır
-    printAllTables();
-
-    // DAT ve HDRM tablolarını .t dosyasına yaz (PDF'de istenen format)
-    FILE *tfp = fopen("output.t", "w");
-    if (tfp) {
-        fprintf(tfp, "=== DIRECT ADDRESS TABLE (DAT) ===\n");
-        for (int i = 0; i < DAT_count; i++) {
-            fprintf(tfp, "%d\n", DAT[i].address);
-        }
-        fprintf(tfp, "\n=== HDRM TABLE ===\n");
-        for (int i = 0; i < HDRM_count; i++) {
-            fprintf(tfp, "%c %s %d\n", HDRMT[i].code, HDRMT[i].symbol, HDRMT[i].address);
-        }
-        fclose(tfp);
-        printf("\nTablolar 'output.t' dosyasina yazildi.\n");
-    } else {
-        printf("\nUYARI: output.t dosyasi olusturulamadi!\n");
-    }
-
-    printf("\n=== PASS 1 TAMAMLANDI ===\n");
-    printf("Partial code 'output.s' dosyasina yazildi.\n");
-
     return 0;
 }
